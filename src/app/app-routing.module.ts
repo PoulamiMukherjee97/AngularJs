d0ef45f0cf1418195636c22e326e97e18ee28286
PoulamiMukherjee97 <pou.mkjee@gmail.com>
2020-10-13 12:55:52 +0530

    Assignment-13/10/2020

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { CustomFormComponent } from './custom-form/custom-form.component';
+import { ReactfunctionsComponent } from './reactfunctions/reactfunctions.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',
+  component:CustomFormComponent
+},{
+path:'students',
+component:ReactfunctionsComponent
+},
+{
+path:'calculator',
+component:CalculatorComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
